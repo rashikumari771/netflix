6ed318ba8e7f6810170eae9004c63c0b74cde09a
Rashi <rashikumari617@gmail.com>
2022-06-20 14:20:12 +0530

    update1

--- firebase.ts
+++ firebase.ts
@@ -8,14 +8,15 @@ import { getAuth } from 'firebase/auth'
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBu-JOto02M274p2DouXPuBt7PiALmUKFw",
-  authDomain: "netflix-b940a.firebaseapp.com",
-  projectId: "netflix-b940a",
-  storageBucket: "netflix-b940a.appspot.com",
-  messagingSenderId: "593285633313",
-  appId: "1:593285633313:web:7cdedfc9aac6da352cda60"
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyCFxwi3rbDZQG7q0BdJBQ0xU6R3kGXVWlQ",
+    authDomain: "netflix-691c2.firebaseapp.com",
+    projectId: "netflix-691c2",
+    storageBucket: "netflix-691c2.appspot.com",
+    messagingSenderId: "587932855327",
+    appId: "1:587932855327:web:193ecddbfee0afd117f92a"
+  };
+  
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
